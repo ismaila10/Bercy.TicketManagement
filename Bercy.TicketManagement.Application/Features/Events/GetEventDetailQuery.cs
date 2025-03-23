52134af2a9b1f50cfbf27425f485355e9a7f636d
ismaila10 <lodia.ismaila@gmail.com>
2025-03-23 16:47:11 +0100

    Add MediatR and AutoMapper Create a request and request handler Add a ServiceCollection extension class

---
+++ Bercy.TicketManagement.Application/Features/Events/GetEventDetailQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Bercy.TicketManagement.Application.Features.Events
+{
+    public class GetEventDetailQuery : IRequest<EventDetailVm>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Bercy.TicketManagement.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Bercy.TicketManagement.Application.Contracts.Persistence;
+using Bercy.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace Bercy.TicketManagement.Application.Features.Events
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery,
+        List<EventListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventsListQueryHandler(IAsyncRepository<Event> eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _eventRepository.ListAllAsync()).OrderBy(x => x.Date);
+
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
